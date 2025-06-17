4e7206ef2efb5654b9530c0bab4aabeb6fd2de10
YanSilva22 <yanviniciusjeba9@gmail.com>
2025-06-17 14:58:21 -0300

    Adiciona leitura de arquivo padrão comandos.txt

--- Program.cs
+++ Program.cs
@@ -3,13 +3,18 @@
     static void Main(string[] args)
     {
         // Verifica se o caminho do arquivo foi passado como argumento
+        string caminhoArquivo = null;
+
         if (args.Length == 0)
         {
-            Console.WriteLine("Uso: programa.exe <caminho_arquivo_comandos.txt>");
-            return;
+            Console.WriteLine("Arquivo não selecionado, usando o arquivo padrão 'comandos.txt'.");
+            caminhoArquivo = "comandos.txt";
+        }
+        else
+        {
+            caminhoArquivo = args[0];
         }
 
-        string caminhoArquivo = args[0];
         ArquivoAVL executor = new ArquivoAVL();
         executor.ExecutarComandos(caminhoArquivo);
     }
